d271ab50eb04bd05030b5218618925cea84451ff
Jo <joepds09@gmail.com>
2023-08-29 13:04:05 +0700

    initial commit

---
+++ components/landing-hero.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import TypewriterComponent from "typewriter-effect";
+import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+export const LandingHero = () => {
+    const { isSignedIn } = useAuth();
+
+    return (
+        <div className="text-white font-bold py-36 text-center space-y-5">
+            <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+                <h1>The Best AI Asisstant for</h1>
+                <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                    <TypewriterComponent
+                        options={{
+                            strings: [
+                                "Conversation AI.",
+                                "Code AI."
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="text-sm md:text-xl font-light text-zinc-400">
+                Learning With Genius Your AI Asisstant.
+            </div>
+            <div>
+                <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+                    <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+                        Start Generating
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
